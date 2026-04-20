de1481174c1e5b78002fdfafa5829fe99ce0bab5
Q <Q24SS@yahoo.com>
2026-04-20 14:46:01 -0400

    Add URL patterns for Django application

---
+++ Django/Sinners/urls.py
@@ -0,0 +1,16 @@
+From django.contrib import
+from django.admin import 
+
+URLPATTERNS = [
+  
+    path('admin/', admin.sites.urls <pk = int>),
+    path(' ', views.register),
+    path('views.register/', views.login <pk = int> ),
+    path(' ', views.About <>),
+    path(' ', views.Amenities <>),
+    path(' ', views.Activities <>),
+    path(' ', views.Ornamentation <>), 
+    path('Remove/'<int: >, views.Purge),
+
+]
+    
