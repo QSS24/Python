08f7eeade65565c3e133d5db82e38c321dbcf8fd
Q <Q24SS@yahoo.com>
2026-04-24 11:45:21 -0400

    Access & Homepage

--- Django/Sinners/Models.py
+++ Django/Sinners/Models.py
@@ -1,1 +1,30 @@
 ##Models for postulated apps 
+
+From django.conf import render
+From PoolthreadsExecutor import Threading as Threds
+Import Queue, OrderedDict, OS,
+From Datetime import Time 
+
+
+Class Access([int, char]:Models, **Kwargs, **args, self):
+     __super__().__init__(self):
+     Meta = Models.model(serialization.jason)
+
+     Def Credentials(Models.model, self):
+             Users = Models.PrimaryKey(Users, default = bool)
+             Emails = models.EmailField(varchar = 256, default = " ")
+             Contact = Models.IntegerField(smallint = 15, null)
+
+
+Class HomePage([int, char]:Pages, **Kwargs, **args, self):
+    __super__().__repr__(self):
+    Meta = { Models.model(serialization.jason)
+           } 
+
+    Def Pagination(Models.model, self):
+        Pages = Queue.Queue(page)
+        Try: 
+            While Pages != none: 
+            
+
+
