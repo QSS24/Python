cf929c7a91e4b8d9d3515d0c09610e32f1857726
Q <Q24SS@yahoo.com>
2026-05-01 13:16:45 -0400

    Refactor models and add ModelsManager class
    Refactor Admins and Access classes, update Credentials method, and add ModelsManager class.

--- Django/Sinners/Models.py
+++ Django/Sinners/Models.py
@@ -4,31 +4,36 @@ From django.conf import render
 From django.contrib.auth.models import models
 From PoolthreadsExecutor import Threading as Threds
 Import Queue, OrderedDict, OS,
-From Datetime import Time 
+Import Time, Lru_Caching 
 
 Class Admins(Models, **Kwargs, **Args, self):
      __super__().__init__(self):
-        Meta = Models.model(self):
-     Try: 
-     Grant_users = Users.Create.Object(Users1, Users@yahoo.com, password=sha256(), is_staff=bool)
      
+     Try: 
+         Grant_users = Users.Create.Object(Users1, Users@yahoo.com, password=sha256(), is_staff=bool)
+         Class Meta:
+            Proxy = Bool
+            Contraints = Model.CheckConstraints(Condition = Models, Defualt = " "); 
 
+Class ModelsManager(Models.Manager, **kwargs, self):
+    __super__().__init__(self):
+    Migration_used = True
 
 Class Access([int, char]:Models, **Kwargs, **args, self):
      __super__().__init__(self):
-     Meta = Models.model(serialization.jason)
 
-     Def Credentials(Models.model, self):
-             Users = Models.PrimaryKey(Users, default = bool, on_delete = models.cascade)
-             Emails = models.ForeighField(varchar = 256, default = null, on_delete = models.cascade)
-             Contact = Models.IntegerField(smallint = 15, null)
-
+     Def Credentials(Models.model, ModelsManager, self):
+             Users = Models.PrimaryKey(Users, default = bool, on_delete = models.cascade);
+             Emails = models.ForeighField(varchar = 256, default = null, on_delete = models.cascade);
+             Contact = Models.IntegerField(smallint = 15, null);
+             Manages = ModelsManager(default = " "); 
+        
+        Class Meta:
+            Proxy = Bool; 
+            Constraints = Models.CheckConstraints(Condition = Models(), default = " " ); 
 
 Class HomePage([int, char]:Pages, **Kwargs, **args, self):
     __super__().__repr__(self):
-    Meta = { Models.model(serialization.jason)
-           } 
-
     Def Pagination(Models.model, self):
         Pages = Queue.Queue(page)
         Try: 
@@ -37,8 +42,8 @@ Class HomePage([int, char]:Pages, **Kwargs, **args, self):
 
 Class Amenities([int, char]:Services, **Kwargs, **args, self):
      __super__().__repr__(self)
-     Meta = { Models.model(serialization.jason) 
-            }
+     Class Meta:
+       Proxy = Bool; 
 
      def Services(self):
 
