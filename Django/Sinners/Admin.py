dabbf6cc4e3345db7adbf63a5312f60f3706343f
Q <Q24SS@yahoo.com>
2026-05-01 12:05:39 -0400

    Implement Grant_Credentials class for user management
    Added a new class for granting credentials and user management.

--- Django/Sinners/Admin.py
+++ Django/Sinners/Admin.py
@@ -1,1 +1,22 @@
 CreatingApps 
+From Django.contrib import Admin
+
+Globals:
+    Queue = Queue.Queue(enumerate())
+    Granted_users = Queue.Queue(enumerate())
+
+
+Class Grant_Credentials(tuples(str, int: Admin.ModelsAdmin), **Kwargs, **args, self):
+    __super__().__init__(self):
+
+    Users_display = 
+    Users_sorted = 
+
+    Try: While Granted_users().is_authenticated && != none: 
+        for i, m, n  in Granted_users() do:
+            Admin.site.register(i)
+    Else:
+        Error as er:
+            printf(' please review and redact ') 
+            
+    Finally: 
