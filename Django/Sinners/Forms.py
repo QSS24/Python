1d9df949663b661a533ec3bafcb3c9270730ae52
Q <Q24SS@yahoo.com>
2026-05-01 11:44:51 -0400

    Add Forms.py with Amenities class definition
    Created a new Forms.py file with an Amenities class for handling form fields.

---
+++ Django/Sinners/Forms.py
@@ -0,0 +1,17 @@
+From Django import Forms
+From PoolThreadsExecutor import Threading as Threds
+Import Time
+Import Queue, OrderedDict Lru_caching
+
+Class Amenities(**Kwargs, **args, Models, self):
+    __super__().__init__(self):
+    @Lru_caching(**Kargs, self): 
+
+    Interfaces = Models.CharacterFields(max_characters = 256);
+    Tools = Models.CharacterFields(default = "bool");
+    Guid = Models.PrimaryKeys(autoincrement= True);
+    Version = Models.CharacterFields(defult = " ");
+
+
+    Class Meta:
+        = 
